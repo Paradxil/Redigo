505eed272ba2d2d45027f863b484c934ce20f0d9
Hunter <hstratton16@gmail.com>
2022-02-16 16:43:59 -0700

    Make track items draggable.

--- app/pages/editor.js
+++ app/pages/editor.js
@@ -1,17 +1,59 @@
-import { Input, Box, Flex, HStack, Stack, Spacer, Button, IconButton, Center, VStack, ButtonGroup, Slider, SliderTrack, SliderFilledTrack, SliderThumb, Container, Menu, MenuButton, MenuList, MenuItem, Text } from "@chakra-ui/react";
+import {
+    Input,
+    Box,
+    Flex,
+    HStack,
+    Stack,
+    Spacer,
+    Button,
+    IconButton,
+    Center,
+    VStack,
+    ButtonGroup,
+    Slider,
+    SliderTrack,
+    SliderFilledTrack,
+    SliderThumb,
+    Container,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    Text
+} from "@chakra-ui/react";
+
 import {
     FiCheck as SaveIcon,
     FiSmartphone as PhoneIcon,
-    FiInstagram as InstagramIcon, 
-    FiYoutube as YoutubeIcon, 
-    FiPlay as PlayIcon, 
-    FiPause as PauseIcon, 
-    FiPlus as AddIcon, 
+    FiInstagram as InstagramIcon,
+    FiYoutube as YoutubeIcon,
+    FiPlay as PlayIcon,
+    FiPause as PauseIcon,
+    FiPlus as AddIcon,
     FiImage as ImageIcon,
     FiVideo as VideoIcon,
-    FiBox as BackgroundIcon
+    FiBox as BackgroundIcon,
+    FiMove as DragIcon
 } from "react-icons/fi";
+
 import Layout from "../components/layout";
+
+import {
+    closestCenter,
+    DndContext,
+    MouseSensor,
+    TouchSensor,
+    useSensors,
+    useSensor
+} from '@dnd-kit/core'
+
+import {
+    SortableContext,
+    useSortable,
+    verticalListSortingStrategy
+} from '@dnd-kit/sortable';
+
+import { CSS } from '@dnd-kit/utilities';
 
 import Animator, { Track } from 'animator';
 import { useEffect, useRef, useState } from "react";
@@ -40,6 +82,22 @@ export default function Editor() {
     const objWrapperRef = useRef(null);
     const videoInputRef = useRef(null);
 
+    const sensors = useSensors(
+        useSensor(MouseSensor, {
+            // Require the mouse to move by 10 pixels before activating
+            activationConstraint: {
+                distance: 10,
+            },
+        }),
+        useSensor(TouchSensor, {
+            // Press delay of 250ms, with tolerance of 5px of movement
+            activationConstraint: {
+                delay: 250,
+                tolerance: 5,
+            },
+        })
+    );
+
     const [videoAnimations, pushVideoAnimation] = useTrack('video');
 
     useEffect(() => {
@@ -55,59 +113,94 @@ export default function Editor() {
         }
     }
 
-    const VideoTrackItem = ({ name }) => {
+    const VideoTrackItem = ({ name, id }) => {
+        const {
+            attributes,
+            listeners,
+            setNodeRef,
+            transform,
+            transition,
+        } = useSortable({ id: id });
+
+        const style = {
+            transform: CSS.Transform.toString(transform),
+            transition,
+        };
+
         return (
-            <Box w='full' bg='white' p={4} rounded={8} shadow='sm'><Text>{name}</Text></Box>
+            <HStack
+                w='full'
+                bg='white'
+                rounded={8}
+                shadow='sm'
+                ref={setNodeRef}
+                {...attributes}
+                style={style}
+                id={id}
+            >
+                <IconButton variant='ghost' {...listeners} icon={<DragIcon/>}/>
+                <Text>{name}</Text>
+            </HStack>
         )
     }
 
     const VideoTrack = () => {
         return (
-            videoAnimations.map(el => <VideoTrackItem key={el.name} name={el.name}/>)
+            <SortableContext
+                items={videoAnimations.map(el => el.name)}
+                strategy={verticalListSortingStrategy}
+            >
+                {videoAnimations.map(el => <VideoTrackItem key={el.name} id={el.name} name={el.name} />)}
+            </SortableContext>
         )
     }
 
     return (
-        <Layout title='Editor' subtitle='New Project' back='/dashboard'>
-            <Container maxW='container.xl'>
-                <HStack bg='white' p={4} rounded={8} marginBottom={4}>
-                    <Input size='lg' placeholder='name'></Input>
-                    <Button size='lg'>Export</Button>
-                    <IconButton icon={<SaveIcon />} size='lg' variant='ghost' />
-                </HStack>
-                <Stack w='full' direction={{ base: 'column', md: 'row' }}>
-                    <VStack bg='white' p={4} rounded={8} flex='1'>
-                        <div id='wrapper' ref={objWrapperRef}>
-                            <canvas width='400px' height='400px' id='canvas' ref={canvasRef}></canvas>
-                        </div>
-                        <HStack>
-                            <ButtonGroup isAttached>
-                                <IconButton icon={<PlayIcon />} />
-                                <IconButton icon={<PauseIcon />} />
-                            </ButtonGroup>
-                            <ButtonGroup isAttached>
-                                <IconButton icon={<PhoneIcon />} />
-                                <IconButton icon={<InstagramIcon />} />
-                                <IconButton icon={<YoutubeIcon />} />
-                            </ButtonGroup>
-                        </HStack>
-                    </VStack>
-                    <Box p={4} flex='1'>
-                        <Menu>
-                            <MenuButton as={Button} leftIcon={<AddIcon />}>Add Media</MenuButton>
-                            <MenuList>
-                                <MenuItem onClick={() => videoInputRef.current.click()} icon={<VideoIcon/>}>Video</MenuItem>
-                                <MenuItem icon={<ImageIcon />}>Image</MenuItem>
-                                <MenuItem icon={<BackgroundIcon />}>Solid Color</MenuItem>
-                            </MenuList>
-                        </Menu>
-                        <Input type='file' ref={videoInputRef} id='video-input' hidden onChange={handleFileUpload} />
-                        <VStack w='full' marginTop={4}>
-                            <VideoTrack/>
+        <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+        >
+            <Layout title='Editor' subtitle='New Project' back='/dashboard'>
+                <Container maxW='container.xl'>
+                    <HStack bg='white' p={4} rounded={8} marginBottom={4}>
+                        <Input size='lg' placeholder='name'></Input>
+                        <Button size='lg'>Export</Button>
+                        <IconButton icon={<SaveIcon />} size='lg' variant='ghost' />
+                    </HStack>
+                    <Stack w='full' direction={{ base: 'column', md: 'row' }}>
+                        <VStack bg='white' p={4} rounded={8} flex='1'>
+                            <div id='wrapper' ref={objWrapperRef}>
+                                <canvas width='400px' height='400px' id='canvas' ref={canvasRef}></canvas>
+                            </div>
+                            <HStack>
+                                <ButtonGroup isAttached>
+                                    <IconButton icon={<PlayIcon />} />
+                                    <IconButton icon={<PauseIcon />} />
+                                </ButtonGroup>
+                                <ButtonGroup isAttached>
+                                    <IconButton icon={<PhoneIcon />} />
+                                    <IconButton icon={<InstagramIcon />} />
+                                    <IconButton icon={<YoutubeIcon />} />
+                                </ButtonGroup>
+                            </HStack>
                         </VStack>
-                    </Box>
-                </Stack>
-            </Container>
-        </Layout>
+                        <Box p={4} flex='1'>
+                            <Menu>
+                                <MenuButton as={Button} leftIcon={<AddIcon />}>Add Media</MenuButton>
+                                <MenuList>
+                                    <MenuItem onClick={() => videoInputRef.current.click()} icon={<VideoIcon />}>Video</MenuItem>
+                                    <MenuItem icon={<ImageIcon />}>Image</MenuItem>
+                                    <MenuItem icon={<BackgroundIcon />}>Solid Color</MenuItem>
+                                </MenuList>
+                            </Menu>
+                            <Input type='file' ref={videoInputRef} id='video-input' hidden onChange={handleFileUpload} />
+                            <VStack w='full' marginTop={4}>
+                                <VideoTrack />
+                            </VStack>
+                        </Box>
+                    </Stack>
+                </Container>
+            </Layout>
+        </DndContext>
     )
 }
