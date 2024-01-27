e2802a61c802c48971ed30e897860ff68cdd2886
paradxil <hstratton16@gmail.com>
2024-01-27 15:47:28 -0700

    Additional conveyor animation options

--- render/examples/conveyors/animation.ts
+++ render/examples/conveyors/animation.ts
@@ -4,9 +4,12 @@ import { data } from "./three-way-problem";
 const { ticks } = data;
 
 const MAP_WIDTH = 9;
-const TILE_SIZE = 40;
+const TILE_SIZE = 50;
 const GAP = 10;
 const SPEED = 500;
+const SHOW_GRID = true;
+const SETUP_INITIAL_TILES = true;
+const LINE_THICKNESS = 4;
 
 export const colors = {
   A: "#ffd8a8",
@@ -43,94 +46,11 @@ const getPos = (index: number) => {
 
   const y = Math.floor(index / MAP_WIDTH);
   const x = index % MAP_WIDTH;
-  return { x: x * (TILE_SIZE + GAP), y: y * (TILE_SIZE + GAP) };
+  return {
+    x: x * (TILE_SIZE + GAP + LINE_THICKNESS),
+    y: y * (TILE_SIZE + GAP + LINE_THICKNESS),
+  };
 };
-
-// const tileTypes = tiles
-//   .split("\n")
-//   .filter((s) => s?.length)
-//   .map((tile) => tile.split(":"))
-//   .reduce(
-//     (out, [id, type]) => ({
-//       ...out,
-//       [id]: type,
-//     }),
-//     {}
-//   );
-
-// const ticks: Record<number, number>[] = moves
-//   .split("---")
-//   .filter((v) => v?.length)
-//   .map((s) =>
-//     s
-//       .split("\n")
-//       .filter((s) => s?.length)
-//       .map((m) => m.split(":").map((m) => +m))
-//       .reduce(
-//         (out, [id, index]) => ({
-//           ...out,
-//           [id]: index,
-//         }),
-//         {}
-//       )
-//   );
-
-// const els = Object.entries(tileTypes).map(([id, type]) => {
-//   const el = document.createElement("div");
-//   el.id = getTileId(id);
-//   el.classList.add("tile");
-//   el.classList.add(getTileClass(type as string));
-//   el.style.opacity = "1";
-//   const { x, y } = getPos(ticks[0][id]);
-//   el.style.left = `${x}px`;
-//   el.style.top = `${y}px`;
-
-//   const iconName = type === "item" ? "box" : `chevrons-${type}`;
-
-//   const icon = icons[iconName].toSvg();
-//   el.innerHTML = icon;
-
-//   container.appendChild(el);
-// });
-
-// const animations = Object.keys(tileTypes)
-//   .sort((a, b) => {
-//     let t1 = getTileType(a[1]);
-//     let t2 = getTileType(b[2]);
-//     if (t1 === TileType.Conveyor && t2 === TileType.Item) {
-//       return 1;
-//     }
-//     if (t1 === TileType.Item && t2 === TileType.Conveyor) {
-//       return -1;
-//     }
-//     return 0;
-//   })
-//   .map((id, index) =>
-//     anime({
-//       autoplay: false,
-//       targets: `#${getTileId(id)}`,
-//       duration: ticks.length * SPEED,
-//       keyframes: [
-//         // {
-//         //   delay: index * 5,
-//         //   opacity: 1,
-//         //   duration: 250,
-//         // },
-//         // {
-//         //   delay: (Object.keys(tileTypes).length - index) * 5,
-//         // },
-//         ...ticks.map((tick) => {
-//           const { x, y } = getPos(tick[id]);
-//           return {
-//             left: `${x}px`,
-//             top: `${y}px`,
-//             duration: SPEED,
-//           };
-//         }),
-//       ],
-//       easing: "easeInOutQuad",
-//     })
-//  );
 
 const animations = [];
 
@@ -139,66 +59,150 @@ let time = {
   tick: 1,
 };
 
-animations.push(
-  anime({
+const timeline = anime.timeline({
+  autoplay: false,
+});
+
+const container = document.getElementById("tilemap");
+container.style.width = `${MAP_WIDTH * (TILE_SIZE + GAP + LINE_THICKNESS)}px`;
+container.style.height = `${MAP_WIDTH * (TILE_SIZE + GAP + LINE_THICKNESS)}px`;
+
+const els: { [key: number]: HTMLDivElement } = {};
+
+if (SHOW_GRID) {
+  Array.from({ length: MAP_WIDTH + 1 }).forEach((_, i) => {
+    const line = document.createElement("div");
+    line.classList.add("gridline", "vertical");
+    let x = i * (TILE_SIZE + GAP + LINE_THICKNESS) - (GAP + LINE_THICKNESS) / 2;
+    let y = -TILE_SIZE / 2 - (GAP + LINE_THICKNESS) / 2;
+    line.style.left = `${x}px`;
+    line.style.top = `${y}px`;
+    line.style.width = `${LINE_THICKNESS}px`;
+    line.style.height = "0px";
+    line.style.opacity = "0";
+
+    container.appendChild(line);
+  });
+
+  Array.from({ length: MAP_WIDTH + 1 }).forEach((_, i) => {
+    const line = document.createElement("div");
+    line.classList.add("gridline", "horizontal");
+    let y = i * (TILE_SIZE + GAP + LINE_THICKNESS) - (GAP + LINE_THICKNESS) / 2;
+    let x = -TILE_SIZE / 2 - (GAP + LINE_THICKNESS) / 2;
+    line.style.left = `${x}px`;
+    line.style.top = `${y}px`;
+    line.style.height = `${LINE_THICKNESS}px`;
+    line.style.width = "0px";
+    line.style.opacity = "0";
+
+    container.appendChild(line);
+  });
+
+  timeline.add({
     autoplay: false,
-    targets: time,
-    interpolation: 1.0,
-    duration: SPEED,
-    loop: true,
-    easing: "easeInOutQuad",
-    loopBegin: () => {
-      time.tick += 1;
-      // if (time.tick >= ticks.length) {
-      //   return;
-      // }
-
-      // const { tiles, positions } = ticks[time.tick];
-
-      // tiles
-      //   .filter(({ id }) => !Object.keys(els).includes(id.toString()))
-      //   .forEach(({ id, tile }) => {
-      //     const container = document.getElementById("tilemap");
-      //     const el = document.createElement("div");
-      //     el.id = getTileId(id);
-      //     el.classList.add("tile");
-      //     el.classList.add("animated");
-      //     el.classList.add(getTileClass(tile));
-      //     el.style.transform = "translateY(-10px)";
-      //     el.style.opacity = "0";
-      //     const { x, y } = getPos(positions[id]);
-      //     el.style.left = `${x}px`;
-      //     el.style.top = `${y}px`;
-
-      //     const iconName = getIcon(tile);
-
-      //     const icon = icons[iconName].toSvg();
-      //     el.innerHTML = icon;
-
-      //     container.appendChild(el);
-
-      //     els[id] = el;
-      //   });
-
-      // Object.entries(els).forEach(([id, el]) => {
-      //   const { x, y } = getPos(positions[id]);
-
-      //   if (x == null || y == null) {
-      //     el.classList.add("deleted");
-      //     return;
-      //   }
-
-      //   // const opacity = Math.max(+el.style.opacity, time.interpolation);
-      //   // const transform = Math.min(
-      //   //   parseInt(el.style.transform.replace(/[^\d]/gi, ""), 10),
-      //   //   10 - time.interpolation * 10
-      //   // );
-      //   el.style.left = `${x}px`;
-      //   el.style.top = `${y}px`;
-      // });
+    targets: ".gridline.vertical",
+    duration: 1000,
+    delay: function (_, index) {
+      return 50 + index * 100;
     },
-  })
-);
+    easing: "easeInOutExpo",
+    opacity: {
+      value: 1,
+      easing: "easeOutExpo",
+    },
+    height: [
+      "0px",
+      `${MAP_WIDTH * (TILE_SIZE + GAP + LINE_THICKNESS) + TILE_SIZE}px`,
+    ],
+  });
+
+  timeline.add({
+    autoplay: false,
+    targets: ".gridline.horizontal",
+    duration: 1000,
+    delay: function (_, index) {
+      return 100 + index * 100;
+    },
+    easing: "easeInOutExpo",
+    opacity: {
+      value: 1,
+      easing: "easeOutExpo",
+    },
+    width: [
+      "0px",
+      `${MAP_WIDTH * (TILE_SIZE + GAP + LINE_THICKNESS) + TILE_SIZE}px`,
+    ],
+  });
+}
+
+if (SETUP_INITIAL_TILES) {
+  const { positions, tiles } = ticks[0];
+  tiles
+    .filter(({ id }) => !Object.keys(els).includes(id.toString()))
+    .forEach(({ id, tile }) => {
+      const el = document.createElement("div");
+      el.id = getTileId(id);
+      el.classList.add("tile");
+      el.classList.add(getTileClass(tile.type));
+      el.style.opacity = "0";
+      const { x, y } = getPos(positions[id]);
+      el.style.left = `${x}px`;
+      el.style.top = `${y}px`;
+      el.style.color = (tile as never as { char: string })?.char
+        ? colors[(tile as never as { char: string }).char]
+        : "";
+
+      const iconName = getIcon(tile);
+
+      const icon = icons[iconName].toSvg({ height: TILE_SIZE * 0.75 });
+      el.innerHTML = icon;
+
+      container.appendChild(el);
+
+      els[id] = el;
+    });
+
+  timeline.add({
+    autoplay: false,
+    targets: ".tile",
+    duration: 1000,
+    delay: function (_, index) {
+      return 100 + index * 200;
+    },
+    easing: "easeOutElastic",
+    elasticity: 650,
+    opacity: {
+      value: 1,
+      easing: "easeOutExpo",
+    },
+    scale: [0, 1],
+  });
+}
+
+const animation = anime({
+  autoplay: false,
+  targets: time,
+  interpolation: 1.0,
+  duration: SPEED,
+  loop: true,
+  easing: "easeInOutQuad",
+  loopBegin: () => {
+    time.tick += 1;
+  },
+});
+
+// animations.push(
+//   anime({
+//     autoplay: false,
+//     targets: ".tile",
+//     duration: 1000,
+//     easing: "easeInOutQuad",
+//     opacity: 1,
+//     delay: function (_, index) {
+//       return 75 + index * 40;
+//     },
+//   })
+// );
 
 // animations.push(
 //   anime({
@@ -219,10 +223,13 @@ animations.push(
 //   })
 // );
 
-const els: { [key: number]: HTMLDivElement } = {};
-
 export function tick(timeStep: number, curTime: number) {
-  animations.forEach((a) => a.tick(curTime));
+  //animations.forEach((a) => a.tick(curTime));
+  timeline.tick(curTime);
+
+  if (timeline.completed) {
+    animation.tick(curTime);
+  }
 
   if (time.tick >= ticks.length) {
     return;
@@ -234,21 +241,21 @@ export function tick(timeStep: number, curTime: number) {
   tiles
     .filter(({ id }) => !Object.keys(els).includes(id.toString()))
     .forEach(({ id, tile }) => {
-      const container = document.getElementById("tilemap");
       const el = document.createElement("div");
       el.id = getTileId(id);
       el.classList.add("tile");
       el.classList.add(getTileClass(tile.type));
-      //el.style.transform = "translateY(-10px)";
       el.style.opacity = "0";
       const { x, y } = getPos(positions[id]);
       el.style.left = `${x}px`;
       el.style.top = `${y}px`;
-      el.style.color = tile?.char ? colors[tile.char] : "";
+      el.style.color = (tile as never as { char: string })?.char
+        ? colors[(tile as never as { char: string }).char]
+        : "";
 
       const iconName = getIcon(tile);
 
-      const icon = icons[iconName].toSvg();
+      const icon = icons[iconName].toSvg({ height: TILE_SIZE * 0.75 });
       el.innerHTML = icon;
 
       container.appendChild(el);
@@ -261,7 +268,6 @@ export function tick(timeStep: number, curTime: number) {
     const { x: lastX, y: lastY } = getPos(lastPositions[id]) ?? { x, y };
 
     if (x == null || y == null) {
-      //el.classList.add("deleted");
       const opacity = Math.min(+el.style.opacity, 1.0 - time.interpolation);
       el.style.opacity = opacity.toString();
       return;
@@ -270,13 +276,9 @@ export function tick(timeStep: number, curTime: number) {
     const ix = (x - lastX) * time.interpolation + lastX;
     const iy = (y - lastY) * time.interpolation + lastY;
     const opacity = Math.max(+el.style.opacity, time.interpolation);
-    const transform = Math.min(
-      parseInt(el.style.transform.replace(/[^\d]/gi, ""), 10),
-      10 - time.interpolation * 10
-    );
+
     el.style.left = `${ix}px`;
     el.style.top = `${iy}px`;
-    //el.style.transform = `translateY(-${transform}px)`;
     el.style.opacity = opacity.toString();
   });
 }
